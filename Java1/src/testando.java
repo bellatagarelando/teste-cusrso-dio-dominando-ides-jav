ffcecae2b8d78013c655b37dc44406ef3a9d37df
Bellatagarelando <isabellacefetmg@gmail.com>
2022-04-19 19:43:12 -0300

    Modificar código

--- Java1/src/testando.java
+++ Java1/src/testando.java
@@ -5,11 +5,17 @@
 			
 			int a=2;
 			int b =3;
+			int c=10;
+		
 			
 			System.out.println("Hello World!" + (a+b));
+			
+			System.out.println("Segunda tentativa" + (a+c));
 			
 			/*olá*/
 		}
 
+		
+		
 
 }
