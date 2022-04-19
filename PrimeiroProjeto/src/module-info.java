5cd7d19b7defbfe44b4ea53898faca6bc63c9011
Bellatagarelando <isabellacefetmg@gmail.com>
2022-04-19 19:36:47 -0300

    adicionar meu primeiro projeto no git

---
+++ PrimeiroProjeto/src/module-info.java
@@ -0,0 +1,17 @@
+module primeiroProjeto {package com.dio;
+
+public class primeiroProjeto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=2;
+		int b =3;
+		
+		System.out.println("Hello World!" + (a+b));
+		
+		/*olá to*/
+	}
+
+}
+}
